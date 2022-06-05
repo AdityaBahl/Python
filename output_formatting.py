1b8dec49f777b4d35d7ce43accd96dd036adca21
AdityaBahl <adityabahl12345@gmail.com>
2022-06-05 12:06:17 +0530

    new file added and updated

--- output_formatting.py
+++ output_formatting.py
@@ -1,5 +1,8 @@
 # format of formatting:-
 #print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+import sys
+from math import pi
+import math
 print(1, 2, 3, 4, sep='#', end='&\n')
 
 # output formatting
@@ -29,3 +32,15 @@ print(float('10'))
 # int('2+3')doesnt work
 print(int('2'+'3'))  # concatinates like a string instead do,
 print(eval('2+3'))
+
+# python import
+# When our program grows bigger, it is a good idea to break it into different modules.
+# A module is a file containing Python definitions and statements. Python modules have a filename and end with the extension .py.
+# Definitions inside a module can be imported to another module or the interactive interpreter in Python. We use the import keyword to do this.
+# For example, we can import the math module by typing the following line:
+print(math.pi)  # imported from line 5
+print(pi)  # alternate way of importing, line 4
+
+
+# While importing a module, Python looks at several places defined in sys.path. It is a list of directory locations.
+sys.path  # from line 3
