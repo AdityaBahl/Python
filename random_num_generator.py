f891653b77550806b26b13c1374446c74ae55c9d
AdityaBahl <adityabahl12345@gmail.com>
2022-08-21 23:47:49 +0530

    Random Number Generator

--- random_num_generator.py
+++ random_num_generator.py
@@ -1,3 +1,4 @@
+""
 import random
 """for random number"""
 # [0.0, 1.0)
@@ -5,24 +6,24 @@ print(random.random())
 """for random number from 0(automatically taken) to 10(here)"""
 print(random.randrange(10))
 """for random number from 1(here) to 10(here)"""
-print(random.randrange(1,10))
+print(random.randrange(1, 10))
 """for random number from 1(here) to 10(here), only odd"""
-print(random.randrange(1,10,2))
+print(random.randrange(1, 10, 2))
 """a<=n<=b(i.e, includes b too, unlike randrange)"""
-print(random.randint(20,30))
+print(random.randint(20, 30))
 """picks a random number from a group of values"""
-print(random.choice([1,2,3,4]))
+print(random.choice([1, 2, 3, 4]))
 """picks a random number from a group of string"""
 print(random.choice("Hello"))
 """picks a random number from a group of values from tupples"""
-print(random.choice((10,20,30,40,50,60)))
+print(random.choice((10, 20, 30, 40, 50, 60)))
 """make a function to generate nums"""
 
 
-def rangen(a,b):
-    return random.randrange(a,b)
+def rangen(a, b):
+    return random.randrange(a, b)
 
 
-print(rangen(10,20))
-print(rangen(20,30))
-print(rangen(30,40))
+print(rangen(10, 20))
+print(rangen(20, 30))
+print(rangen(30, 40))
