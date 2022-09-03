eb82f7c4e583a9eef318dc3adbfe191b1634f7d3
AdityaBahl <adityabahl12345@gmail.com>
2022-09-03 18:31:42 +0530

    File Handling Operations

--- output_formatting.py
+++ output_formatting.py
@@ -1,5 +1,5 @@
 """
-this program showcases output formatting operat
+this program showcases output formatting operati
 """
 # format of formatting:-
 # print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
